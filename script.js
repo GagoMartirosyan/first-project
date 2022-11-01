13eab61c3c288da35c6463681db428dd27fae52b
Gago <martirosyangag24@gmail.com>
2022-11-01 19:53:30 +0400

    Jquery code

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-fetch('./manifest.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json.name));
+fetch("./manifest.json")
+  .then((response) => response.json())
+  .then((json) => console.log(json.name));
+
+$(document).ready(function () {
+  $(".btn").click(function () {
+    alert("Value:" + $("#test").val());
+  });
+});
